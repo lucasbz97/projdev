9e800d20584d6f1762f67af57f1f6b33b5a4304b
Lucas Souza <36299479+lucasbz97@users.noreply.github.com>
2019-05-20 16:43:31 -0300

    Update DAO.php

--- class/DAO.php
+++ class/DAO.php
@@ -13,7 +13,8 @@ class DAO
     public function Inserir($novo)
     {
         $comm_insert = "insert into usuario (ID,EMAIL,SENHA) values(1,?,teste)";
-
+        $comm_insert = "insert into questoes (questao, alternativa, checkbox) values (test,?,1)";
+        
         $stm = mysqli_prepare($this->con, $comm_insert);
         $stm->bind_param('s',$email);
         $email = $novo;
